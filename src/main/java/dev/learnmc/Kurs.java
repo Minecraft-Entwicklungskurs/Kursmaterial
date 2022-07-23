efc9f2f3b426ef95588aec7e13eef1a509816565
Tjorven Liebe <32434395+Tjorven-Liebe@users.noreply.github.com>
2022-07-23 19:15:03 +0200

    initial commit

---
+++ src/main/java/dev/learnmc/Kurs.java
@@ -0,0 +1,12 @@
+package dev.learnmc;
+
+public class Kurs {
+
+    public static void main(String[] args) {
+        int integer = 1;
+        int integerTwo = 2;
+
+        System.out.println(integer + integerTwo);
+    }
+
+}
